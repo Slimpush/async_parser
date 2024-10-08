617e61dc30c46ffd82e896b0b2416179dad3c89b
Kirill Rudenko <slimpkowl@gmail.com>
2024-10-08 14:54:26 +0700

    last_link_update

--- async_parser/main.py
+++ async_parser/main.py
@@ -1,6 +1,8 @@
 import asyncio
 import aiohttp
-from parsers.url_collector import collect_all_trade_links
+import time
+from datetime import datetime
+from parsers.html_parser import get_trade_link
 from parsers.excel_parser import parse_xls_data
 from db_manager import create_tables, insert_data
 
@@ -28,10 +30,20 @@ async def handle_link(link: str, date: str) -> None:
 
 async def process_data() -> None:
     try:
-        links = await collect_all_trade_links()
-        await create_tables()
-        tasks = [handle_link(link, date) for link, date in links]
-        await asyncio.gather(*tasks)
+        starttime = time.time()
+        url = 'https://spimex.com/markets/oil_products/trades/results/?page=page-1'
+        link = await get_trade_link(url)
+
+        if link:
+            await create_tables()
+            date = datetime.now().strftime("%d.%m.%Y")
+            await handle_link(link, date)
+        else:
+            print("Не удалось получить последнюю ссылку.")
+        endtime = time.time()
+        time_count = endtime - starttime
+        minutes, seconds = divmod(time_count, 60)
+        print(f"Время выполнения process_data: {int(minutes)} минут и {seconds:.2f} секунд")
 
     except Exception as e:
         print(f"Произошла ошибка при обработке данных: {e}")
--- async_parser/utils/validator.py
+++
@@ -1,3 +0,0 @@
-def is_valid_year(date_str: str) -> bool:
-    year = int(date_str.split('.')[2])
-    return year == 2023 or year == 2024
